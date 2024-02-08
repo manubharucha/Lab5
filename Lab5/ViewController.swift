1bb95d19063ae4e99a0e6f181438ae6e7373443b
manu bharucha <manubharucha1631@gmail.com>
2024-02-08 11:43:43 -0800

    Initial Commit

---
+++ Lab5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab5
+//
+//  Created by user240208 on 2/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //IOS
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
